dea5268c17682d1f9ebf226bddf5bb7d8a781d3a
nirman12 <nirmankhanal477@gmail.com>
2026-02-09 10:52:43 +0545

    notes

---
+++ notes.js
@@ -0,0 +1,20 @@
+/*
+How github actions automate these workflows?
+    - When something happens in or to your repository
+    - Automatic actions are executed in response
+*/
+
+/*
+Is it just another CI/CD tool?
+    Use same tool instead of third-party integration
+    Setup the pipeline is easy
+    Tool for developers
+
+
+Why is the setup easier?
+    Integration with other technologies is important
+    
+
+    */ 
+
+ 
